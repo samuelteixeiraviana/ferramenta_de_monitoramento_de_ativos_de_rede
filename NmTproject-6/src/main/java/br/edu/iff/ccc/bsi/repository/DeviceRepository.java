e286e9c804fa357057a52dcba571afae6f25b47a
samuelteixeiraviana <samuel.teixeira@gsuite.iff.edu.br>
2025-03-24 20:27:54 -0300

    Add files via upload

--- NmTproject-6/src/main/java/br/edu/iff/ccc/bsi/repository/DeviceRepository.java
+++ NmTproject-6/src/main/java/br/edu/iff/ccc/bsi/repository/DeviceRepository.java
@@ -1,9 +1,17 @@
 package br.edu.iff.ccc.bsi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.edu.iff.ccc.bsi.entities.Device;
 
 public interface DeviceRepository extends JpaRepository<Device, Long> {
-
+	
+	@Query("select d from Device d where d.name = ?1")
+	List<Device> findByName(String name);
+	
+	@Query("select d from Device d where d.address = ?1")
+	Device findByAddress(String address);
 }
